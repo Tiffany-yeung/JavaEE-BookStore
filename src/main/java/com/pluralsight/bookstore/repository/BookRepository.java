e7e4d5d806434a9e13e98ade39a1b9881bbf7d19
Tiffany Yeung <tiffany.yeung@qa.com>
2019-02-09 15:30:36 +0000

    Added JPA entity manager to perform CRUD operations and queries, and JTA for transaction demarcation

---
+++ src/main/java/com/pluralsight/bookstore/repository/BookRepository.java
@@ -0,0 +1,54 @@
+package com.pluralsight.bookstore.repository;
+
+import com.pluralsight.bookstore.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+//Ensures it will not invoke more than one transaction
+//so no conflict between 2 writing methods at the same time
+@Transactional(SUPPORTS)
+public class BookRepository {
+	//Injection points
+	//Add the JPA Entity Manager and persistence unit
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager em;
+
+    //Business methods
+    //CRUD operations
+    //read only
+    public Book find(Long id) {
+        return em.find(Book.class, id);
+    }
+    
+    //JQPL Queries
+    public List<Book> findAll() {
+		//takes a JPQL query and returns a TypedQuery interface
+	    TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b ORDER BY b.title DESC", Book.class);
+	    return query.getResultList();
+	}
+	
+	public Long countAll() {
+	    TypedQuery<Long> query = em.createQuery("SELECT COUNT(b) FROM Book b", Long.class);
+	    return query.getSingleResult();
+	}
+
+	//write only
+	//required = making transactions mandatory
+    @Transactional(REQUIRED)
+    public Book create(Book book) {
+        em.persist(book);
+        return book;
+    }
+
+    @Transactional(REQUIRED)
+    public void delete(Long id) {
+        em.remove(em.getReference(Book.class, id));
+    }
+}
